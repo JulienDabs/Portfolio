1fc48e53fcb98d208f51739741c154a3a72b7970
JulienDabs <j.dabadie81@gmail.com>
2022-03-27 16:35:51 +0200

    change font family style

--- app/views/layout/header.tpl.php
+++ app/views/layout/header.tpl.php
@@ -8,6 +8,10 @@
 
     <link rel="stylesheet" href="<?= $viewData['assetsBaseUri'] ; ?>/css/reset.css">
     <link rel="stylesheet" href="<?= $viewData['assetsBaseUri'] ; ?>/css/style.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 
     <nav>
         <ul>
